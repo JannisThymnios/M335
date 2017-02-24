7d79ed8d8a2f244aad1f5ae213f7b9d19193f229
JannisThymnios <j.thymnios@gmail.com>
2017-02-24 11:57:51 +0100

    Tabelle Score hinzugefügt

--- M335ReactionTimeAppNew/platforms/browser/www/js/database.js
+++ M335ReactionTimeAppNew/platforms/browser/www/js/database.js
@@ -26,15 +26,39 @@ function Beclick(){
 }
 
 
+function ScoreSpeichern(){
+    
+    var scoreData = {
+        Name: "Muster",
+        Mode: "Classic",
+        Difficulty: "Einfach",
+        Score: "5000"
+    };
 
-/*function NameUser(){
-    var UserRef = firebase.database().ref().child('User').key(;
+    var newScoreKey = firebase.database().ref().child('Score').push().key;
 
-    UserRef.on("value", snap => {
+    var updates2 = {};
+    updates2['/Score/' + newScoreKey] = scoreData;
+
+    return firebase.database().ref().update(updates2);
+}
+
+
+
+
+function ScoreLesen(){
+    var ScoreRef = firebase.database().ref().child('Score');
+
+    UserRef.on("child.added", snap => {
         var Username = snap.child("Name").val();
+        var Mode = snap.child("Mode").val();
+        var Schwieigkeit = snap.child("Difficulty").val();
+        var score = snap.child("Score").val();
+
+        //weiterer Code kommt noch sobald die Scoreboardseite existiert.
 
         window.alert(Username);
     });
 
     
-}*/
+}
