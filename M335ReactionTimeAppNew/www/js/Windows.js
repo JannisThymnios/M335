d1366851de2b6fc2efa8a622c4a640a213a5fce7
Vanilleschlecker <Vanilleschlecker@gmail.com>
2017-03-02 16:13:13 +0100

    cooooooooooooooooooooooomit

--- M335ReactionTimeAppNew/www/js/Windows.js
+++ M335ReactionTimeAppNew/www/js/Windows.js
@@ -34,6 +34,16 @@ function GoToScoreboard() {
 function GoToHelp() {
     HideAllContainer();
     UnHideByID("help");
+    if(GameMode == "Classic")
+    {
+        HideByID("MusicHelp");
+        UnHideByID("ClassicHelp");
+    }
+    else
+    {
+        HideByID("ClassicHelp");
+        UnHideByID("MusicHelp");
+    }
 };
 //------------------------------------------------------------------------------------
 //Zeigt Schwierigkeit
